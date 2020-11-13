3fd87a85b919f903bccbdaa9a4bf46fee03f3aed
Jose <jose@email.com>
2020-11-13 15:29:20 +0100

    Commit 2 Se crea Toast entrar al Activity 2

--- app/src/main/java/corcoles/jose/pac1/activitys/TablaActivity.kt
+++ app/src/main/java/corcoles/jose/pac1/activitys/TablaActivity.kt
@@ -2,11 +2,14 @@ package corcoles.jose.pac1.activitys
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import corcoles.jose.pac1.R
 
 class TablaActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_tabla)
+        Toast.makeText(this, "Estas en el Activity de Cración de tabla",
+            Toast.LENGTH_LONG).show()
     }
 }
