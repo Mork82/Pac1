5e50fd1d4c2dc82da6e3287d7ee7d8a68f4ea582
Jose <jose@email.com>
2020-11-13 17:03:36 +0100

    Commit 3 Rom dao creada

--- app/src/main/java/corcoles/jose/pac1/activitys/TablaActivity.kt
+++ app/src/main/java/corcoles/jose/pac1/activitys/TablaActivity.kt
@@ -1,16 +1,22 @@
 package corcoles.jose.pac1.activitys
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
 import corcoles.jose.pac1.R
+import kotlinx.android.synthetic.main.activity_tabla.*
 
 class TablaActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_tabla)
-        Toast.makeText(this, "Estas en el Activity de Cración de tabla",
+        Toast.makeText(this, "Estas en el Activity de Creación de tabla",
             Toast.LENGTH_LONG).show()
+        btnVolver.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
 
 
     }
